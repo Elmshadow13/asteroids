b31e63366e29287bb5f8cebb4346cdac590a3ef1
Elmshadow13 <emmamashburn13@gmail.com>
2026-04-08 18:33:17 -0400

    added timing

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
+        dt = clock.tick(60) / 1000
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        
 if __name__ == "__main__":
     main()
