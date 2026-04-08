6b3dfff32d7254de5e5594ae2eeb5672eb997a14
Elmshadow13 <emmamashburn13@gmail.com>
2026-04-08 19:11:06 -0400

    draw spaceship

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
+from player import Player
+
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
     while True:
         dt = clock.tick(60) / 1000
         log_state()
@@ -16,6 +20,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         
 if __name__ == "__main__":
